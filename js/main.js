966b5887088242397bf7b3a13ae22b990b000b02
Alaa4eg-h <Alaa4eg@outlook.com>
2021-07-11 08:54:20 +0200

    Testi section

---
+++ js/main.js
@@ -0,0 +1,17 @@
+/**
+ * Testimonials slider
+ */
+new Swiper(".swiper-container", {
+  speed: 600,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  slidesPerView: "auto",
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+    clickable: true,
+  },
+});
